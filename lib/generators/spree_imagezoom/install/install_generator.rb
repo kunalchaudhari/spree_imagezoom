0ede0e065cdb996f7aa297598d46a87d40c7a806
Kunal Chaudhari <kunalchaudhari@gmail.com>
2012-02-23 16:48:19 +0530

    append to js and css

--- lib/generators/spree_imagezoom/install/install_generator.rb
+++ lib/generators/spree_imagezoom/install/install_generator.rb
@@ -4,26 +4,12 @@ module SpreeImagezoom
 
       def add_javascripts
         append_file "app/assets/javascripts/store/all.js", "//= require store/spree_imagezoom\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_imagezoom\n"
       end
 
       def add_stylesheets
         inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_imagezoom\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_imagezoom\n", :before => /\*\//, :verbose => true
       end
 
-      def add_migrations
-        run 'bundle exec rake railties:install:migrations FROM=spree_imagezoom'
-      end
-
-      def run_migrations
-         res = ask "Would you like to run the migrations now? [Y/n]"
-         if res == "" || res.downcase == "y"
-           run 'bundle exec rake db:migrate'
-         else
-           puts "Skiping rake db:migrate, don't forget to run it!"
-         end
-      end
     end
   end
 end
