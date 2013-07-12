4abe239936a4b45e1193e5061b93582466bceca1
Kunal Chaudhari <kunalchaudhari@gmail.com>
2013-07-12 10:10:08 +0530

    fix undefined title on zoom image

--- app/overrides/product_image_zoom.rb
+++ app/overrides/product_image_zoom.rb
@@ -4,5 +4,5 @@
 Deface::Override.new(:virtual_path => "spree/products/_image",
                       :name => "product-image-zoom",
                       :surround => "code[erb-loud]:contains('product_image')") do
-                        "<a href='<%= @product.images.empty? ? \"javascript:void(0);\" : @product.images.first.attachment.url(:original) %>' rel='product-gallery' class='<%= @product.images.empty? ? \"javascript:void(0);\" : \"spree_jqzoom\" %>'><%= render_original %></a><div class='clearfix'></div>"
-                      end
+                        "<a href='<%= @product.images.empty? ? \"javascript:void(0);\" : @product.images.first.attachment.url(:original) %>' title='<% @product.name %>' rel='product-gallery' class='<%= @product.images.empty? ? \"javascript:void(0);\" : \"spree_jqzoom\" %>'><%= render_original %></a><div class='clearfix'></div>"
+end
