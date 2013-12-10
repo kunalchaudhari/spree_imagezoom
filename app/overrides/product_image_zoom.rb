1e60778986d486da8b5cdfcc4d70962e814684ba
Micah Roberson <micah.roberson@gmail.com>
2013-12-10 13:10:21 -0800

    change selector class to large_image

--- app/overrides/product_image_zoom.rb
+++ app/overrides/product_image_zoom.rb
@@ -3,6 +3,6 @@
 # </a>
 Deface::Override.new(:virtual_path => "spree/products/_image",
                       :name => "product-image-zoom",
-                      :surround => "code[erb-loud]:contains('product_image')") do
+                      :surround => "code[erb-loud]:contains('large_image')") do
                         "<a href='<%= @product.images.empty? ? \"javascript:void(0);\" : @product.images.first.attachment.url(:original) %>' rel='product-gallery' class='<%= @product.images.empty? ? \"javascript:void(0);\" : \"spree_jqzoom\" %>'><%= render_original %></a><div class='clearfix'></div>"
                       end
