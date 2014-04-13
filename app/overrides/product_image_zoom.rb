03395fe249c0390e003d2b619b58301f657e3a80
David Collom <david@collom.co.uk>
2014-04-13 11:55:01 +0100

    revert back to view's and update selected to work with latest deface

--- app/overrides/product_image_zoom.rb
+++ app/overrides/product_image_zoom.rb
@@ -3,6 +3,5 @@
 # </a>
 Deface::Override.new(:virtual_path => "spree/products/_image",
                       :name => "product-image-zoom",
-                      :surround => "code[erb-loud]:contains('product_image')") do
-                        "<a href='<%= @product.images.empty? ? \"javascript:void(0);\" : @product.images.first.attachment.url(:original) %>' title='<%= @product.name %>' rel='product-gallery' class='<%= @product.images.empty? ? \"javascript:void(0);\" : \"spree_jqzoom\" %>'><%= render_original %></a><div class='clearfix'></div>"
-end
+                      :surround => "erb[loud]:contains('product_image')",
+                      partial: 'spree/products/zoom/image' )
