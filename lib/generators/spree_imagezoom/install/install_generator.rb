0724b16ea3c4bd6bb1f4cc0c881fe93fe6e5ec4e
David Collom <david@collom.co.uk>
2014-04-13 11:33:06 +0100

    update paths to follow new structure within spree 2.2+

--- lib/generators/spree_imagezoom/install/install_generator.rb
+++ lib/generators/spree_imagezoom/install/install_generator.rb
@@ -3,11 +3,11 @@ module SpreeImagezoom
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_imagezoom\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require frontent/spree_imagezoom\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_imagezoom\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require frontent/spree_imagezoom\n", :before => /\*\//, :verbose => true
       end
 
     end
