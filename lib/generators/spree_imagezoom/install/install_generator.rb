787d504f374d11f0aeef138fdcbc7043280bf423
endenis <endenis@gmail.com>
2014-09-06 22:11:11 +0200

    Fixed a typo in the install generator

--- lib/generators/spree_imagezoom/install/install_generator.rb
+++ lib/generators/spree_imagezoom/install/install_generator.rb
@@ -3,11 +3,11 @@ module SpreeImagezoom
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require frontent/spree_imagezoom\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require frontend/spree_imagezoom\n"
       end
 
       def add_stylesheets
-        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require frontent/spree_imagezoom\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require frontend/spree_imagezoom\n", :before => /\*\//, :verbose => true
       end
 
     end
